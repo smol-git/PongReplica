97f128bca1caf8199fe6449c424d82dd86dd559b
Sohail <sohailahmedwork1@gmail.com>
2023-04-15 16:56:02 +0500

    updated

--- PongReplica/Assets/BallController.cs
+++ PongReplica/Assets/BallController.cs
@@ -4,23 +4,17 @@ using UnityEngine;
 
 public class BallController : MonoBehaviour
 {
-    [SerializeField] 
-    float speed = 5f;
-    private Rigidbody2D rb;
+    public float speed = 10f;
+    private Rigidbody2D rb2d;
 
-    private void Start()
+    void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
-        rb.velocity = new Vector2(speed, Random.Range(-1f, 1f));
+        rb2d = GetComponent<Rigidbody2D>();
     }
 
-    private void OnCollisionEnter2D(Collision2D other)
+    void FixedUpdate()
     {
-        if (other.gameObject.CompareTag("Paddle"))
-        {
-            float velY = rb.velocity.y;
-            velY = velY / 2f + other.gameObject.GetComponent<Rigidbody2D>().velocity.y / 3f;
-            rb.velocity = new Vector2(-rb.velocity.x, velY);
-        }
+        Vector2 vel = rb2d.velocity.normalized;
+        rb2d.velocity = vel * speed;
     }
 }
